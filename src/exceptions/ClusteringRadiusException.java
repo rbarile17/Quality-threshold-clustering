ea8312284c50ea99264782e5d2a0c581510038c8
Roberto Barile <roberto.barile98@gmail.com>
2019-05-17 16:16:51 +0200

    Removed exceptions folder

--- src/exceptions/ClusteringRadiusException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class ClusteringRadiusException extends Exception {
-	public ClusteringRadiusException() { }
-}
--- src/exceptions/EmptyDatasetException.java
+++
@@ -1,5 +0,0 @@
-package exceptions;
-
-public class EmptyDatasetException extends Exception {
-	public EmptyDatasetException() { }
-}
