34a03dd109bfb8d47b08fb07212102cd51d3b8bc
Pasquale De Marinis <pasquale199876@hotmail.it>
2019-07-15 15:06:37 +0200

    exception

--- QTClientFX/src/application/Main.java
+++ QTClientFX/src/application/Main.java
@@ -1,12 +1,16 @@
 package application;
 	
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 
 public class Main extends Application {
@@ -19,8 +23,15 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(IOException e) {
-			e.printStackTrace();
+		} catch(IOException | NullPointerException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace(pw);
+			alert.setTitle("Exception");
+			alert.setHeaderText("Exception Thrown");
+			alert.setContentText(sw.toString());
+			alert.show();
 		}
 	}
 	
