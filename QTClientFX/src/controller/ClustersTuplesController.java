db7bd1ed687e8f0a0dc074661c239aef473d04f3
Roberto Barile <roberto.barile98@gmail.com>
2019-07-17 17:02:38 +0200

    Nuovo controller

---
+++ QTClientFX/src/controller/ClustersTuplesController.java
@@ -0,0 +1,23 @@
+package controller;
+
+import java.util.List;
+
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import model.ServerModel;
+
+public class ClustersTuplesController extends Controller{
+	private ServerModel serverModel;
+	
+	@FXML
+	private TableView<List<StringProperty>> table;
+	
+	public void initialize(ServerModel serverModel, LinkedList<LinkedList<String>> centroids) {
+		this.serverModel = serverModel;
+		
+		
+		
+	}
+
+}
