a9e9c45f5e3ef5e80221ccc93627d1d80393e768
Roberto Barile <roberto.barile98@gmail.com>
2019-07-18 19:13:15 +0200

    filtro multiplo con menubutton

--- QTClientFX/src/controller/ClustersTuplesController.java
+++ QTClientFX/src/controller/ClustersTuplesController.java
@@ -1,32 +1,34 @@
 package controller;
 
-import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.TreeSet;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckMenuItem;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
-import model.ServerModel;
-import utility.ExceptionAlert;
 
 public class ClustersTuplesController extends Controller{
 	private LinkedList<LinkedList<String>> centroids;
 	private LinkedList<List<List<String>>> tuples;
-	
-	@FXML
-	private TextField filter;
+	LinkedList<String> names;
 	
 	@FXML
 	private Button applyFilter;
+	
+	@FXML
+	private Button removeFilter;
+	
+	@FXML
+	private MenuButton filter;
 	
 	@FXML
 	private TableView<List<StringProperty>> table;
@@ -34,6 +36,7 @@ public class ClustersTuplesController extends Controller{
 	public void initialize(LinkedList<LinkedList<String>> centroids, LinkedList<String> names, LinkedList<List<List<String>>> tuples) {
 		this.centroids = centroids;
 		this.tuples = tuples;
+		this.names = names;
 		
 		table.setRowFactory(tv -> new TableRow<List<StringProperty>>() {
 		    @Override
@@ -49,6 +52,19 @@ public class ClustersTuplesController extends Controller{
 		    }
 		});
 		
+		this.setFilter();
+		this.fillTable();
+	}
+	
+	private void setFilter() {
+		for(int i=0; i<centroids.size(); i++) {
+			CheckMenuItem menuItem = new CheckMenuItem();
+			menuItem.setText(String.valueOf(i));
+			filter.getItems().add(menuItem);
+		}
+	}
+	
+	private void fillTable() {
 		ObservableList<List<StringProperty>> list = FXCollections.observableArrayList();
 		
 		TableColumn<List<StringProperty>, String> col = new TableColumn<List<StringProperty>, String>("Centroid Index");
@@ -94,40 +110,62 @@ public class ClustersTuplesController extends Controller{
 		table.setItems(list);		
 	}
 	
+	private void fillTable(TreeSet<Integer> notFiltered) {
+		ObservableList<List<StringProperty>> list = FXCollections.observableArrayList();
+		
+		TableColumn<List<StringProperty>, String> col = new TableColumn<List<StringProperty>, String>("Centroid Index");
+		col.setCellValueFactory(data -> data.getValue().get(0));
+		table.getColumns().add(col);
+		col = new TableColumn<List<StringProperty>, String>("Distance");
+		col.setCellValueFactory(data -> data.getValue().get(1));
+		table.getColumns().add(col);
+		int i=2;
+		for(String s : names) {
+			final int j = i;
+			TableColumn<List<StringProperty>, String> c = new TableColumn<List<StringProperty>, String>(s);
+			c.setCellValueFactory(data -> data.getValue().get(j));
+			table.getColumns().add(c);
+			i++;
+		}
+					
+		int k=0;
+		for(int j : notFiltered) {
+			List<StringProperty> oList = new LinkedList<StringProperty>();
+			oList.add(0, new SimpleStringProperty(k+" "));
+			oList.add(1, new SimpleStringProperty("0.0"));
+			i = 2;
+			for(String s : centroids.get(j)) {
+				oList.add(i, new SimpleStringProperty(s));
+				i++;
+			}	
+			list.add(oList);
+			
+			for(List<String> example : tuples.get(j)) {
+				oList = new LinkedList<StringProperty>();
+				oList.add(0, new SimpleStringProperty(String.valueOf(k)));
+				i = 1;
+				for(String s : example) {
+					oList.add(i, new SimpleStringProperty(s));
+					i++;
+				}
+				list.add(oList);
+			}
+			k++;
+		}
+		table.setItems(list);		
+	}
 	
 	public void applyFilterClick() {
-		if (filter.getText().equals("")) {
-			new ExceptionAlert("Filter", "Filter cannot be empty", AlertType.WARNING);
-			return;
-		}
+		TreeSet<Integer> notFiltered = 
+			filter.getItems().stream()
+			.filter(menuItem -> CheckMenuItem.class.isInstance(menuItem) && CheckMenuItem.class.cast(menuItem).isSelected())
+			.mapToInt(menuItem -> Integer.parseInt(menuItem.getText()))
+			.collect(TreeSet::new, TreeSet::add, TreeSet::addAll);
 			
-		int centroidIndex = Integer.parseInt(filter.getText());
-		if(centroidIndex >= centroids.size() || centroidIndex < 0) {
-			new ExceptionAlert("Filter", "This centroid doesn't exist", AlertType.WARNING);	
-			return;
-		}
-		ObservableList<List<StringProperty>> list = FXCollections.observableArrayList();
-		table.setItems(list);
-		
-		List<StringProperty> oList = new LinkedList<StringProperty>();
-		oList.add(0, new SimpleStringProperty(centroidIndex+" "));
-		oList.add(1, new SimpleStringProperty("0.0"));
-		int j = 2;
-		for(String s : centroids.get(centroidIndex)) {
-			oList.add(j, new SimpleStringProperty(s));
-			j++;
-		}	
-		list.add(oList);
-		
-		for(List<String> example : tuples.get(centroidIndex)) {
-			oList = new LinkedList<StringProperty>();
-			oList.add(0, new SimpleStringProperty(String.valueOf(centroidIndex)));
-			j = 1;
-			for(String s : example) {
-				oList.add(j, new SimpleStringProperty(s));
-				j++;
-			}
-			list.add(oList);
-		}
+		this.fillTable(notFiltered);
+	}
+	
+	public void removeFilterClick() {
+		this.fillTable();
 	}
 }
