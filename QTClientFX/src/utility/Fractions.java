0151045b21a5017d7321ab2bb11163124e7da550
Roberto Barile <roberto.barile98@gmail.com>
2019-07-19 21:08:58 +0200

    classe con costanti

---
+++ QTClientFX/src/utility/Fractions.java
@@ -0,0 +1,11 @@
+package utility;
+
+import com.sun.glass.ui.Screen;
+
+public class Fractions {
+	public static final double HEIGHT_1 = 0.85;
+	public static final double HEIGHT_2 = 0.02;
+	public static final double WIDTH_1 = 0.025;
+	public static final double SCREEN_HEIGHT = Screen.getMainScreen().getHeight();
+	public static final double SCREEN_WIDTH = Screen.getMainScreen().getWidth();
+}
